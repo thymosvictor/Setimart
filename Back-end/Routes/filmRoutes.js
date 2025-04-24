1444df638a5102e9f5e105f08867932a92605f8e
Thymos Victor <166143287+thymosvictor@users.noreply.github.com>
2025-04-24 22:21:40 +0000

    terminar o pwa

--- Back-end/Routes/filmRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getFilms } from '../controllers/filmController.js'; // Correct
-
-
-const router = Router();
-
-router.get('/', getFilms);
-
-export default router;
